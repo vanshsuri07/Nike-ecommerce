bafbf6f16eb11144d03a9fc26838fcfc04b46480
Vanshsuri07 <vanshsuri07@gmai.com>
2025-08-31 12:39:56 +0530

    Project Setup

---
+++ drizzle/0000_flippant_maginty.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "products" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"description" text,
+	"price" numeric(10, 2) NOT NULL,
+	"image" text
+);
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1 className='text-heading-1 font-jost'>
+      Home
+    </h1>
+  )
+}
+
+export default page
