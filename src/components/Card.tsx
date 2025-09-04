17bbf2310f70699d3b5c63c7dcef543b8392e32e
Vanshsuri07 <vanshsuri07@gmai.com>
2025-09-04 23:49:27 +0530

    Update Hero Section

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,36 +10,38 @@ const Card: React.FC<CardProps> = ({ product }) => {
   const { name, price, image, category, colors, bestseller } = product;
 
   return (
-    <article className={`group rounded-xl bg-light-200 ring-1 ring-light-300 transition-colors  hover:ring-dark-500 `}>
+    <article className="group flex flex-col h-full rounded-lg bg-light-200 ring-1 ring-light-300 transition-colors hover:ring-dark-500">
       <div className="relative">
-        {/* The z-index is added to ensure the tag is on top of the image. */}
         {bestseller && (
           <div className="absolute top-0 left-0 bg-orange text-red text-caption font-bold px-3 py-1 m-4 rounded-full z-10">
             Best Seller
           </div>
-        )}
-        {image && (
-          <div className="relative h-80 w-full">
-            <Image 
-            src={image} 
-            alt={name} 
-            fill
-            sizes='(min-width: 1280px) 360px, (min-width: 1024px) 300px, (min-width: 640px) 45vw, 90vw' 
-            className="object-fit transition-transform duration-300 group-hover:scale-105" />
-          </div>
-        )}
-      </div>
-      <div className="p-4">
-        <div className="flex justify-between items-start">
-          <div>
-            <h3 className="text-heading-3 text-dark-900">{name}</h3>
-            <p className="text-body text-dark-700 mt-1">{category}</p>
-            <p className="text-body text-dark-700 mt-1">{colors}</p>
-          </div>
-          <span className="text-lead text-dark-900">${price}</span>
-        </div>
-      </div>
-    </article>
+    )}
+
+    {image && (
+      <div className="relative h-60 w-full overflow-hidden">
+  <Image 
+    src={image} 
+    alt={name} 
+    fill
+    className="object-cover transition-transform duration-300 group-hover:scale-105"
+    sizes="(min-width: 1280px) 360px, (min-width: 1024px) 300px, (min-width: 640px) 45vw, 90vw" 
+  />
+</div>
+    )}
+  </div>
+
+  <div className="flex flex-col justify-between flex-grow p-4">
+  <div className="mb-2">
+    <h3 className="text-heading-3 text-dark-900">{name}</h3>
+    <p className="text-body text-dark-700 mt-1">{category}</p>
+    <p className="text-body text-dark-700 mt-1">{colors}</p>
+  </div>
+  <span className="text-lead text-dark-900">${price}</span>
+</div>
+
+</article>
+
   );
 };
 
