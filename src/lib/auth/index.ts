53e2b8764ab1402ecc8d36c67c49ef219c97325f
Vanshsuri07 <vanshsuri07@gmai.com>
2025-09-06 00:03:56 +0530

    Fix Auth

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -3,19 +3,21 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from '../../db/index' // adjust if no TS alias
 import * as schema from "../../lib/db/schema"; // exports user, accounts, etc.
+import { user, session, account, verifications} from "../../lib/db/schema";
+
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
-    schema: schema,
+    schema: { user, session, account, verifications },
   }),
+  user: {
+    modelName: "user",
+  },
+  advanced: {
+    generateId: () => crypto.randomUUID(), // Generate proper UUIDs
+  },
   emailAndPassword: {
     enabled: true,
   },
- advanced: {
-    generateId: () => {
-      // Generate proper UUID v4
-      return crypto.randomUUID();
-    }
-  }
 });
